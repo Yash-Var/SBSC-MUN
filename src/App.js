ff951aa5983ac9fc68f7babe2664974ecb12edca
Yash-Var <yashvarshney7011@gmail.com>
2023-04-02 02:34:22 +0530

    add the registration from

--- src/App.js
+++ src/App.js
@@ -6,13 +6,25 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Index from "./components/HomePage/Index";
 import Footer from "./components/Footer/Footer";
 import Team from "./components/Team/Team";
+import styled from "styled-components";
+import Notice from "./components/Notice";
+import notice from "./components/notice.json";
+import Layout from "./components/Team/Layout";
+const NoticeSection = styled.div`
+  display: ${({ isVisible }) => (isVisible ? "block" : "none")};
+`;
 function App() {
   return (
     <div>
-      {/* <Navbar /> */}
-      {/* <Index /> */}
+      <Navbar />
+      <Index />
+      <Layout>
+        <NoticeSection isVisible={notice.notice.isVisible}>
+          <Notice />
+        </NoticeSection>
+      </Layout>
       <Team />
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 }
