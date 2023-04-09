4a29a2db268727103f5c995f2c4c19e8448ebec5
Yash-Var <yashvarshney7011@gmail.com>
2023-04-09 21:37:53 +0530

    add

--- src/components/Iconbar/Iconbar.js
+++ src/components/Iconbar/Iconbar.js
@@ -3,35 +3,43 @@ import "./Style.css";
 export const Iconbar2 = () => {
   return (
     <div className="kush">
-    <div class="icon-bar">
-      <a href="#" class="facebook" target="_kushagra">
-        click here to visit <i class="fab fa-facebook-square"></i>
-      </a>
+      <div class="icon-bar">
+        {/* <a href="#" class="facebook" target="_kushagra">
+          click here to visit <i class="fab fa-facebook-square"></i>
+        </a> */}
 
-      <a href="#" class="telegram" target="_kushagra">
-        click here to visit <i class="fab fa-telegram-plane"></i>
-      </a>
+        <a
+          href="https://twitter.com/sbscmunsociety?t=ZYUJMljl9mTMyjdGZ7ZD0w&s=09"
+          class="telegram"
+          target="_kushagra"
+        >
+          click here to visit <i class="fa-brands fa-twitter fab"></i>
+        </a>
 
-      <a
-        href="https://www.instagram.com/varshneykushagra/"
-        class="instagram"
-        target="_kushagra"
-      >
-        click here to visit <i class="fab fa-instagram"></i>
-      </a>
+        <a
+          href="https://instagram.com/munsoc_sbsc?igshid=YmMyMTA2M2Y="
+          class="instagram"
+          target="_kushagra"
+        >
+          click here to visit <i class="fab fa-instagram"></i>
+        </a>
 
-      <a href="https://www.linkedin.com/in/kushagra-varshney0511/" class="linkedin" target="_kushagra">
-        click here to visit <i class="fab fa-linkedin"></i>
-      </a>
+        <a
+          href="https://www.linkedin.com/company/the-sbsc-mun-society/"
+          class="linkedin"
+          target="_kushagra"
+        >
+          click here to visit <i class="fab fa-linkedin"></i>
+        </a>
 
-      <a
-        href="https://www.youtube.com/channel/UCYkEBehq2f_ansIvazNKTkw"
-        class="youtube"
-        target="_kushagra"
-      >
-        click here to visit <i class="fab fa-youtube-square"></i>
-      </a>
-    </div>
+        <a
+          href="https://youtube.com/@sbscmun?feature=shares"
+          class="youtube"
+          target="_kushagra"
+        >
+          click here to visit <i class="fab fa-youtube-square"></i>
+        </a>
+      </div>
     </div>
   );
 };
